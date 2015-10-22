1b7f2d0ce829c1c6af57d35ec9cbdd67c58f7c77
jjiayu <jjiayu889@gmail.com>
2015-10-22 01:25:46 +0100

    finally color filters finished

--- Code/IPAB-SLMC-rss-sandbox-63337b433557/vision/vision.py
+++ Code/IPAB-SLMC-rss-sandbox-63337b433557/vision/vision.py
@@ -45,6 +45,8 @@ class robot_vision:
         self.IO.cameraGrab()
         self.img = self.IO.cameraRead()
 
+        self.color_list = ['red','green','blue','yellow','orange','white']
+        self.object_detected_list = [False,False,False,False,False]
         #setting color filter ranges
         #white filter, hsv_white = [[[30,2,255]]]
         #self.lower_white = numpy.array([0,0,50])
@@ -56,24 +58,36 @@ class robot_vision:
         self.upper_green = numpy.array([80,255,255])
         self.boundry_green = [self.lower_green,self.upper_green]
 
-        self.lower_yellow = numpy.array([20,130,130])
-        self.upper_yellow = numpy.array([40,255,255])
+        '''
+        self.lower_black = numpy.array([0,0,0])
+        self.upper_black = numpy.array([255,255,40])
+        self.boundry_black = [self.lower_black,self.upper_black]
+        '''
+        self.lower_yellow = numpy.array([20,130,120])
+        self.upper_yellow = numpy.array([35,255,255])
         self.boundry_yellow = [self.lower_yellow,self.upper_yellow]
 
         #red, hue 0-5
-        self.lower_red = numpy.array([0,10,10])
+        self.lower_red = numpy.array([0,100,100])
         self.upper_red = numpy.array([10,255,255])
         self.boundry_red = [self.lower_red,self.upper_red]
 
-        #blue hue: light blue: 97, normal blue:85
-        self.lower_blue = numpy.array([80,100,100])
-        self.upper_blue = numpy.array([130,255,200])
+        self.lower_white = numpy.array([0,0,240])
+        self.upper_white = numpy.array([255,15,255])
+        self.boundry_white = [self.lower_white,self.upper_white]
+
+        self.lower_blue = numpy.array([80,100,50])
+        self.upper_blue = numpy.array([130,255,255])
         self.boundry_blue = [self.lower_blue,self.upper_blue]
+
+        self.lower_orange = numpy.array([10,150,150])
+        self.upper_orange = numpy.array([20,255,255])
+        self.boundry_orange = [self.lower_orange,self.upper_orange]
 
         #self.boundry_list = [self.boundry_yellow]
         #self.boundry_list = [self.boundry_green]
         #self.boundry_list = [self.boundry_red]
-        self.boundry_list = [self.boundry_red, self.boundry_green, self.boundry_blue]
+        self.boundry_list = [self.boundry_red, self.boundry_green, self.boundry_blue,self.boundry_yellow,self.boundry_orange,self.boundry_white]
         #self.boundry_list = [self.boundry_white]
 
     def Set_Resolution(self):
@@ -91,10 +105,12 @@ class robot_vision:
         #self.IO.imshow('grey',grey)
         #time.sleep(1)
 
-    def ColorFilter(self,boundry):
+    def ColorFilter(self,color):
 
         interested_contour_areas = []
         interested_contours = []
+        final_contours = []
+        boundry = self.boundry_list[self.color_list.index(color)]
         hsv_image = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv_image, boundry[0], boundry[1])
         im2,contours, hierachy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
@@ -112,12 +128,58 @@ class robot_vision:
                 interested_contour_areas.append(cv2.contourArea(contour))
                 interested_contours.append(contour)
 
-        
+
         if len(interested_contours)>0:
+            print "------------------------"
+            print color, 'object detected'
+            print "------------------------"
             #print "find objects"
             for contour in interested_contours:
-                x,y,w,h = cv2.boundingRect(contour)
-                cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,0),2)
+
+                #cv2.drawContours(self.img, contour, -1, (0,255,0), 2)
+                hull = cv2.convexHull(contour,returnPoints = False)
+                #print 'contour ', interested_contours.index(contour), 'hull is', len(hull)
+                defects = cv2.convexityDefects(contour,hull)
+                #print 'distances'
+                #print '---------------------------------------------------------'
+                #print 'shape of defects', defects.shape
+                total_distance = 0
+                for i in range(defects.shape[0]):
+                    s,e,f,d = defects[i,0]
+                    distance = d/256.0
+                    #print 'distance for every point ----' , distance
+                    total_distance = total_distance+distance
+                #print "Total distance of the hull"
+                #print total_distance
+                ratio_distances_volumn = distance/cv2.contourArea(contour)
+                #print "distances/Volumn"
+                #print ratio_distances_volumn
+                if ratio_distances_volumn < 0.0015:
+                    final_contours.append(contour)
+                    print '========================================='
+                    print "Total distance of the hull of the object"
+                    print total_distance
+                    print "Corresponding distances/Volumn"
+                    print ratio_distances_volumn
+            print '=================================================='
+            print 'number of ', color, 'objects', len(final_contours)
+
+
+                    #start = tuple(contour[s][0])
+                    #end = tuple(contour[e][0])
+                    #far = tuple(contour[f][0])
+                    #cv2.line(self.img,start,end,[0,255,0],2)
+                    #cv2.circle(self.img,far,5,[0,0,255],-1)
+                #x,y,w,h = cv2.boundingRect(contour)
+                #cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,0),2)
+
+        """
+        if len(interested_contours) == 1:
+            print color, 'object detected'
+            self.object_detected_list[self.color_list.index(color)]=True
+            print interested_contour_areas
+        """
+
 
 
         #print 'interested contour size'
@@ -126,22 +188,112 @@ class robot_vision:
 
         #cv2.drawContours(self.img, contours, -1, (0,255,0),2)
         #res = cv2.bitwise_and(self.img,self.img,mask=mask)
-
+        cv2.drawContours(self.img, final_contours, -1, (0,255,0), 2)
+        cv2.drawContours(self.img, interested_contours, -1, (0,255,0), 2)
         self.IO.imshow('image',self.img)
         #return max_contour
 
-        return interested_contour_areas
+        return color, len(final_contours)
 
+    def Black_filter(self):
+
+        interested_contour_areas = []
+        interested_contours = []
+        final_contours = []
+        #boundry = self.boundry_list[self.color_list.index(color)]
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        mask = cv2.inRange(gray, 15, 50)
+        im2,contours, hierachy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+        #max_contour = contours[0]
+
+        """
+        for contour in contours:
+            if cv2.contourArea(contour) > cv2.contourArea(max_contour):
+                max_contour = contour
+        """
+
+        #print 'contours sizes'
+        for contour in contours:
+            if cv2.contourArea(contour)>100.0:
+                interested_contour_areas.append(cv2.contourArea(contour))
+                interested_contours.append(contour)
+
+
+        if len(interested_contours)>0:
+            print "------------------------"
+            print 'black ', 'object detected'
+            print "------------------------"
+            #print "find objects"
+            for contour in interested_contours:
+
+                #cv2.drawContours(self.img, contour, -1, (0,255,0), 2)
+                hull = cv2.convexHull(contour,returnPoints = False)
+                #print 'contour ', interested_contours.index(contour), 'hull is', len(hull)
+                defects = cv2.convexityDefects(contour,hull)
+                #print 'distances'
+                #print '---------------------------------------------------------'
+                #print 'shape of defects', defects.shape
+                total_distance = 0
+                for i in range(defects.shape[0]):
+                    s,e,f,d = defects[i,0]
+                    distance = d/256.0
+                    #print 'distance for every point ----' , distance
+                    total_distance = total_distance+distance
+                #print "Total distance of the hull"
+                #print total_distance
+                ratio_distances_volumn = distance/cv2.contourArea(contour)
+                #print "distances/Volumn"
+                #print ratio_distances_volumn
+                if ratio_distances_volumn < 0.0015:
+                    final_contours.append(contour)
+                    print '========================================='
+                    print "Total distance of the hull of the object"
+                    print total_distance
+                    print "Corresponding distances/Volumn"
+                    print ratio_distances_volumn
+            print '=================================================='
+            print 'number of ', 'black', 'objects', len(final_contours)
+
+
+                    #start = tuple(contour[s][0])
+                    #end = tuple(contour[e][0])
+                    #far = tuple(contour[f][0])
+                    #cv2.line(self.img,start,end,[0,255,0],2)
+                    #cv2.circle(self.img,far,5,[0,0,255],-1)
+                #x,y,w,h = cv2.boundingRect(contour)
+                #cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,0),2)
+
+        """
+        if len(interested_contours) == 1:
+            print color, 'object detected'
+            self.object_detected_list[self.color_list.index(color)]=True
+            print interested_contour_areas
+        """
+
+
+
+        #print 'interested contour size'
+        #for contour in interested_contours:
+        #    print cv2.contourArea(contour)
+
+        #cv2.drawContours(self.img, contours, -1, (0,255,0),2)
+        #res = cv2.bitwise_and(self.img,self.img,mask=mask)
+        cv2.drawContours(self.img, final_contours, -1, (0,255,0), 2)
+        cv2.drawContours(self.img, interested_contours, -1, (0,255,0), 2)
+        self.IO.imshow('image',self.img)
+        return final_contours
 
     def find_objects(self):
 
         objects = []
-        for boundry in self.boundry_list:
-            object_list = self.ColorFilter(boundry)
+        for color in self.color_list:
+            object_list = self.ColorFilter(color)
             if len(object_list) == 0:
                 objects.append([0])
             else:
                 objects.append(object_list)
+
+        #@self.Black_filter()
 
         return objects
 
